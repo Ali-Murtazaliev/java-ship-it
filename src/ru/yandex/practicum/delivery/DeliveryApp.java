0a9216e2f4c736905d8acf5eb449c860448716bb
Ali <amurtazaliev450@gmail.com>
2026-04-26 17:08:04 -0700

    Go

--- src/ru/yandex/practicum/delivery/DeliveryApp.java
+++ src/ru/yandex/practicum/delivery/DeliveryApp.java
@@ -1,5 +1,3 @@
-package ru.yandex.practicum.delivery;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -8,6 +6,10 @@ public class DeliveryApp {
 
     private static final Scanner scanner = new Scanner(System.in);
     private static List<Parcel> allParcels = new ArrayList<>();
+    private static List<Trackable> trackableParcels = new ArrayList<>();
+    private static ParcelBox<StandardParcel> standardParcelsBox = new ParcelBox<>(100);
+    private static ParcelBox<FragileParcel> fragileParcelsBox = new ParcelBox<>(200);
+    private static ParcelBox<PerishableParcel> perishableParcelsBox = new ParcelBox<>(300);
 
     public static void main(String[] args) {
         boolean running = true;
@@ -25,6 +27,12 @@ public class DeliveryApp {
                 case 3:
                     calculateCosts();
                     break;
+                case 4:
+                    reportStatus();
+                    break;
+                case 5:
+                    getParcelsFromBox();
+                    break;
                 case 0:
                     running = false;
                     break;
@@ -39,22 +47,99 @@ public class DeliveryApp {
         System.out.println("1 — Добавить посылку");
         System.out.println("2 — Отправить все посылки");
         System.out.println("3 — Посчитать стоимость доставки");
+        System.out.println("4 — Сообщить местоположение посылок");
+        System.out.println("5 — Показать содержимое коробки");
         System.out.println("0 — Завершить");
     }
 
     // реализуйте методы ниже
 
     private static void addParcel() {
-        // Подсказка: спросите тип посылки и необходимые поля, создайте объект и добавьте в allParcels
+        System.out.println("Какой тип посылки вы хотите отправить?");
+        System.out.println("1 — Стандартная посылка");
+        System.out.println("2 — Хрупкая посылка");
+        System.out.println("3 — Скоропортящаяся посылка");
+        int newParcelType = Integer.parseInt(scanner.nextLine());
+        System.out.println("Введите описание посылки");
+        String newParcelDescription = scanner.nextLine();
+        System.out.println("Введите вес посылки");
+        int newParcelWeight = Integer.parseInt(scanner.nextLine());
+        System.out.println("Введите адрес доставки");
+        String newParcelAddress = scanner.nextLine();
+        System.out.println("День отправки");
+        int newParcelSendDay = Integer.parseInt(scanner.nextLine());
+
+        switch (newParcelType) {
+            case 1:
+                StandardParcel standardParcel = new StandardParcel(newParcelDescription,
+                        newParcelWeight, newParcelAddress, newParcelSendDay);
+                allParcels.add(standardParcel);
+                standardParcelsBox.addParcel(standardParcel);
+                break;
+            case 2:
+                FragileParcel fragileParcel = new FragileParcel(newParcelDescription,
+                        newParcelWeight, newParcelAddress, newParcelSendDay);
+                allParcels.add(fragileParcel);
+                fragileParcelsBox.addParcel(fragileParcel);
+                trackableParcels.add(fragileParcel);
+                break;
+            case 3:
+                System.out.println("Введите срок хранения");
+                int newParcelTimeToLive = Integer.parseInt(scanner.nextLine());
+                PerishableParcel perishableParcel = new PerishableParcel(newParcelDescription,
+                        newParcelWeight, newParcelAddress, newParcelSendDay, newParcelTimeToLive);
+                allParcels.add(perishableParcel);
+                perishableParcelsBox.addParcel(perishableParcel);
+                break;
+        }
     }
 
     private static void sendParcels() {
-        // Пройти по allParcels, вызвать packageItem() и deliver()
+        for (Parcel parcel : allParcels) {
+            parcel.packageItem();
+            parcel.deliver();
+        }
     }
 
     private static void calculateCosts() {
-        // Посчитать общую стоимость всех доставок и вывести на экран
+        int allParcelsPrice = 0;
+        for (Parcel parcel : allParcels) {
+            allParcelsPrice += parcel.calculateDeliveryCost();
+        }
+        System.out.println("Общая стоимость доставки всех посылок - " + allParcelsPrice);
+
     }
 
+    private static void reportStatus() {
+        System.out.println("Введите новое местоположение:");
+        String newLocation = scanner.nextLine();
+        for (Trackable parcel : trackableParcels) parcel.reportStatus(newLocation);
+    }
+    private static void getParcelsFromBox() {
+        System.out.println("Какой тип посылок вы хотите посмотреть?");
+        System.out.println("1 — Стандартная посылка");
+        System.out.println("2 — Хрупкая посылка");
+        System.out.println("3 — Скоропортящаяся посылка");
+        int parcelBoxType = Integer.parseInt(scanner.nextLine());
+        switch (parcelBoxType) {
+            case 1:
+                for (StandardParcel parcel : standardParcelsBox.getParcelBox()){
+                    System.out.println(parcel.getDescription());
+                }
+                break;
+            case 2:
+                for (FragileParcel parcel : fragileParcelsBox.getParcelBox()){
+                    System.out.println(parcel.getDescription());
+                }
+                break;
+            case 3:
+                for (PerishableParcel parcel : perishableParcelsBox.getParcelBox()){
+                    System.out.println(parcel.getDescription());
+                }
+                break;
+        }
+
+    }
 }
+
 
--- src/ru/yandex/practicum/delivery/Parcel.java
+++ src/ru/yandex/practicum/delivery/Parcel.java
@@ -1,5 +1,65 @@
-package ru.yandex.practicum.delivery;
+import java.lang.constant.Constable;
 
-public class Parcel {
-    //добавьте реализацию и другие необходимые классы
+public abstract class Parcel {
+    private String description;
+    private int weight;
+    private String deliveryAddress;
+    private int sendDay;
+    static final int STANDARD_PARCEL_PRICE = 2;
+    static final int PERISHABLE_PARCEL_PRICE = 3;
+    static final int FRAGILE_PARCEL_PRICE = 4;
+
+
+    public Parcel(String description, int weight, String deliveryAddress, int sendDay) {
+        this.description = description;
+        this.weight = weight;
+        this.deliveryAddress = deliveryAddress;
+        this.sendDay = sendDay;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getDeliveryAddress() {
+        return deliveryAddress;
+    }
+
+    public void setDeliveryAddress(String deliveryAddress) {
+        this.deliveryAddress = deliveryAddress;
+    }
+
+    public int getSendDay() {
+        return sendDay;
+    }
+
+    public void setSendDay(int sendDay) {
+        this.sendDay = sendDay;
+    }
+
+    public void packageItem() {
+        System.out.println("Посылка " + getDescription() +  " упакована");
+    }
+
+    public void deliver() {
+        System.out.println("Посылка " + getDescription() + " доставлена по адресу " +  getDeliveryAddress());
+    }
+
+    public int calculateDeliveryCost() {
+        return getWeight() * getDeliveryPrice();
+    }
+
+    public abstract int getDeliveryPrice();
 }
